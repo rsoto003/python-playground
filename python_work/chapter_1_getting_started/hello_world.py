c4e1855467c1f7c1c5966054e1cce1f197c8c481
Ryan Soto <ryansotodev@gmail.com>
2024-02-14 23:54:24 -0800

    learned about f-strings and adding whitespaces to my code

--- python_work/chapter_1_getting_started/hello_world.py
+++
@@ -1,8 +0,0 @@
-print("Hello Python World!")
-
-message = "Hello Python World!"
-message_two = "This is my second Python program."
-
-
-print(message)
-
---
+++ python_work/full_name.py
@@ -0,0 +1,8 @@
+# f strings - 
+first_name = 'ryan'
+last_name = 'soto'
+full_name = f"{first_name} {last_name}"
+day_of_the_week = "wednesday"
+message = f"Hello, my name is {full_name.title()}.\nHow are you today?\nAwesome. Great to hear it.\n\tHave a great rest of your {day_of_the_week.title()}. :)"
+
+print(message)
