ff83b43cc67b9a357169577da62d936c9bd7bdf7
Ryan Soto <ryansotodev@gmail.com>
2024-02-15 23:47:02 -0800

    #1: completed try_it_yourself exercises on page 25.

---
+++ python_work/chapter_2_try_it_yourself/famous_quote.py
@@ -0,0 +1,11 @@
+author = 'kobe bryant'
+team = 'Los Angeles Lakers'
+position = 'Shooting Guard'
+number = '24'
+
+quote_1 = 'Once you know what failure feels like, determination chases success.'
+quote_2 = "The most important thing is you must put everybody on notice that you're here and you are for real."
+
+print(f"{quote_1}\n\t-{author.title()}, {position}, #{number}, {team}")
+
+print(f"\n{quote_2}\n\t-{author.title()}, {position}, #{number}, {team}")
---
+++ python_work/chapter_2_try_it_yourself/file_extensions.py
@@ -0,0 +1,6 @@
+filename = 'python_notes.txt'
+
+file_without_extension = filename.removesuffix('.txt')
+
+print(filename)
+print(file_without_extension)
