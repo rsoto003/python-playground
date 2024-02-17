10731d8db24b6a5ebb89a172753d9f39acd2c06c
Ryan Soto <ryansotodev@gmail.com>
2024-02-16 23:31:50 -0800

    chapter_3 work: created folder structure and completed first round of try_it_yourself exercises

---
+++ python_work/chapter_3/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale', 'redline', 'huffy', 'specialized']
+
+print(bicycles)
+
+index = bicycles.index('huffy')
+
+print(bicycles[index])
+
+print(bicycles[-1])
---
+++ python_work/chapter_3_try_it_yourself/greetings.py
@@ -0,0 +1,7 @@
+names = ['rocky', 'luna', 'cooper', 'sadie']
+greeting = 'How are you?'
+
+print(f"Hello, {names[0].title()}! {greeting}")
+print(f"Hello, {names[1].title()}! {greeting}")
+print(f"Hello, {names[2].title()}! {greeting}")
+print(f"Hello, {names[3].title()}! {greeting}")
