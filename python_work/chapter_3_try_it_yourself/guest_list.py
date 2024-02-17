8d869318c8d25bd705ef850e8852d0d54c647379
Ryan Soto <ryansotodev@gmail.com>
2024-02-16 23:54:25 -0800

    completed try it yourself exercise on page 41, completed

---
+++ python_work/chapter_3_try_it_yourself/guest_list.py
@@ -0,0 +1,5 @@
+# try it yourself exercise on page 41
+guest_list = ['kobe bryant', 'nipsey hussle', 'lebron james', 'shohei ohtani', 'caleb williams', 'denzel washington']
+
+for guest in guest_list:
+    print(f"Hi {guest.title()}, would you like to attend my dinner party?")
