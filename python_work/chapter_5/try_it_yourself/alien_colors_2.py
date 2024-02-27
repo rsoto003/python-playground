4d934ad99199b9f7975fddd7862b1ddf0a272432
Ryan Soto <ryansotodev@gmail.com>
2024-02-26 23:26:52 -0800

    completed try it yourself exercises 5-3, 5-4

---
+++ python_work/chapter_5/try_it_yourself/alien_colors_2.py
@@ -0,0 +1,13 @@
+# Try it yourself on page 84
+alien_color = 'blue'
+
+if alien_color == 'blue':
+    print('nice shot! +5 points!')
+else:
+    print('close but no cigar')
+
+
+if alien_color == 'green':
+    print('congrats, you defeated the aliens! +100 points!')
+else: 
+    print('you failed to save earth. try again?')
