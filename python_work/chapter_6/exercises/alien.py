c4b5a6de9ae2f9d3060e936e8dd6c4ffe753ec07
Ryan Soto <ryansotodev@gmail.com>
2024-03-01 21:51:35 -0800

    started learning dictionaires via alien example

--- python_work/chapter_6/exercises/alien.py
+++ python_work/chapter_6/exercises/alien.py
@@ -1,4 +1,18 @@
 alien_0 = { 'color': 'green', 'points': 5}
 
-print(alien_0['color'])
-print(alien_0['points'])
+alien_1 = { 'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+
+# move alien to the right
+# determine HOW FAR by the alien's current speed
+
+if alien_1['speed'] == 'slow':
+    x_increment = 1
+elif alien_1['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+# the new position is the old position plus the increment.
+alien_1['x_position'] = alien_1['x_position'] + x_increment
+
+print(f"New position: {alien_1['x_position']}")
