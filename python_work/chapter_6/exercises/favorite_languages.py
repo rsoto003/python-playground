d5ca77d0313f09240a782f93ae26c1eb64d2b4c0
Ryan Soto <ryansotodev@gmail.com>
2024-03-03 23:07:35 -0800

    better naming for 'key, value' in loop for better readability, added an ifelse for favorite languages func

--- python_work/chapter_6/exercises/favorite_languages.py
+++ python_work/chapter_6/exercises/favorite_languages.py
@@ -13,7 +13,10 @@ ryans_fav_language = favorite_languages.get('ryan', 'Sorry, that user does not e
 print(ryans_fav_language)
 
 def list_everyones_favorite_languages(list):
-    for key, value in list.items():
-        print(f"\n {key}'s favorite language is {value}")
+    for name, language in list.items():
+        if language == 'python':
+            print(f"wow, {name.title()}, my favorite language is {language.title()} too!")
+        else:
+            print(f"oh, {name.title()}, don't worry, i'm sure {language.title()} is great as well.")
 
 list_everyones_favorite_languages(favorite_languages)
