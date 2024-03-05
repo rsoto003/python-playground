e1552986ecea4420784f6f860bdd72ee2164dad6
Ryan Soto <ryansotodev@gmail.com>
2024-03-04 23:55:36 -0800

    finished 6-5, started 6-6

---
+++ python_work/chapter_6/try_it_yourself/rivers.py
@@ -0,0 +1,8 @@
+rivers = {
+    'Mississippi': 'Mississippi, USA',
+    'Volga': 'Europe',
+    'Nile': 'South America'
+}
+
+for river_name, location in rivers.items():
+    print(f"{river_name} is located in {location}")
